5515d9d9a49992cf1bf4400309b4c5781b24eb30
paawan3000 <93308514+paawan3000@users.noreply.github.com>
2024-04-02 13:54:11 +0530

    Remove and ignore node_modules

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path  = require("path");
+const ejsMate = require("ejs-mate");
+
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,'public')));
+const moment = require('moment');
+const FormattedDate1 = moment().format('dddd, MMM D');
+const port = 3000;
+app.get("/home",(req,res)=>{
+    res.render("index/home.ejs",{FormattedDate1});
+})
+
+app.get("/",(req,res)=>{
+    res.send("Page Coming Soon!");
+})
+
+app.get("/journal",(req,res)=>{
+    res.render("index/journal.ejs");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`);
+})
